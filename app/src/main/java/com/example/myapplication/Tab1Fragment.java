8f19c577766282adbfc495dcb26e5f3722c2e1f4
gus092 <guswl092@gmail.com>
2019-06-27 10:04:14 +0900

    grid1
    made simple avocado grid

--- app/src/main/java/com/example/myapplication/Tab1Fragment.java
+++ app/src/main/java/com/example/myapplication/Tab1Fragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,19 +10,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Tab1Fragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Tab1Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Tab1Fragment extends Fragment {
+    public Context thiscontext;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        super.onCreate(savedInstanceState);
+
+        thiscontext = container.getContext();
+
+        View view = inflater.inflate(R.layout.fragment_tab1,container,false);
+
+        GridView gridView = (GridView)view.findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(thiscontext));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long ld) {
+                Intent i = new Intent(getActivity(),FullImageActivity.class );
+                //Intent i = new Intent(getApplicationContext(),FullImageActivity.class );
+                i.putExtra("id",position);
+                startActivity(i);
+            }
+        });
+
+        return view;
+
     }
 }
