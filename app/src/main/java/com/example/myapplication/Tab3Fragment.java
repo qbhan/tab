ecf5850681188caa734be82b394cb55caae46420
qbhan <52191815+qbhan@users.noreply.github.com>
2019-07-01 21:56:01 +0900

    Add betting in game
    We can bet for the game in scale of 5 with initial point of 50.

--- app/src/main/java/com/example/myapplication/Tab3Fragment.java
+++ app/src/main/java/com/example/myapplication/Tab3Fragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.SystemClock;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +19,10 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import android.os.CountDownTimer;
 
 
 /**
@@ -32,16 +36,29 @@ import java.util.Random;
 public class Tab3Fragment extends Fragment {
 
     ImageView iv_card_left, iv_card_right;
-    TextView tv_score_left, tv_score_right;
-    Button b_deal, b_reset;
+    TextView tv_score_left, tv_score_right, tv_bet_left, tv_bet_right;
+    Button b_deal, b_reset, b_leftbet, b_rightbet;
+
 
     Random r, rr;
 
-    int leftScore, rightScore = 0;
-    String[] shapes = {"clubs", "diamonds", "hearts", "spades"};
 
+    int deckCount = 0;
+    int betLeft = 0;
+    int betRight = 0;
+    int leftScore = 50;
+    int rightScore = 50;
+    String[] shapes = {"clubs", "diamonds", "hearts", "spades"};
+    List<String> leftDeck = new ArrayList<String>();
+    List<String> rightDeck = new ArrayList<String>();
+    List<String> wholeDeck = new ArrayList<String>();
+    List<Integer> leftDeckNumbers = new ArrayList<Integer>();
+    List<Integer> rightDeckNumbers = new ArrayList<Integer>();
 
     MediaPlayer mp = new MediaPlayer();
+
+    public Tab3Fragment() {
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,46 +76,154 @@ public class Tab3Fragment extends Fragment {
         iv_card_right = (ImageView) view.findViewById(R.id.iv_card_right);
         tv_score_left = (TextView) view.findViewById(R.id.tv_score_left);
         tv_score_right = (TextView) view.findViewById(R.id.tv_score_right);
+        tv_bet_left = (TextView) view.findViewById(R.id.bet_score_left);
+        tv_bet_right = (TextView) view.findViewById(R.id.bet_score_right);
         b_deal = (Button) view.findViewById(R.id.b_deal);
         b_reset = (Button) view.findViewById(R.id.b_reset);
+        b_leftbet = (Button) view.findViewById(R.id.left_bet);
+        b_rightbet = (Button) view.findViewById(R.id.right_bet);
 
 //        b_deal.setOnClickListener();
 
         r = new Random();
         rr = new Random();
 
+        shuffleDeck();
 
+        b_leftbet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (betLeft <= leftScore-5) {
+                    betLeft += 5;
+                    tv_bet_left.setText(String.valueOf(betLeft));
+                } else {
+                    Toast.makeText(requireContext(), "YOU CAN'T BET MORE THAN YOUR SCORE!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        b_rightbet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (betRight <= rightScore-5) {
+                    betRight += 5;
+                    tv_bet_right.setText(String.valueOf(betRight));
+                } else {
+                    Toast.makeText(requireContext(), "YOU CAN'T BET MORE THAN YOUR SCORE!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         b_deal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //generate the two card numbers
-                int leftCard = r.nextInt(13) + 2; //this is for cards 2 - 14
-                int rightCard = r.nextInt(13) + 2; //this is for cards 2 - 14
-                String leftShape = shapes[rr.nextInt(3)];
-                String rightShape = shapes[rr.nextInt(3)];
+//                int leftNumber = r.nextInt(13) + 2; //this is for cards 2 - 14
+//                int rightNumber = r.nextInt(13) + 2; //this is for cards 2 - 14
+//                String leftShape = shapes[rr.nextInt(3)];
+//                String rightShape = shapes[rr.nextInt(3)];
+//                String leftCard = leftShape + leftNumber;
+//                String rightCard = rightShape + rightNumber;
+                if (betRight==0 || betLeft==0) {
+                    Toast.makeText(requireContext(), "PLEASE BET AT LEAST 5!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else {
+                    if (deckCount == 26) {
+//                    Toast.makeText(requireContext(), "RAN OUT OF CARDS!!! PLEASE RESET!!!", Toast.LENGTH_LONG).show();
+                        if (leftScore > rightScore) {
+                            Toast.makeText(requireContext(), "LEFT SIDE WON!!", Toast.LENGTH_LONG).show();
+                            deckCount = 27;
+                        } else if (leftScore < rightScore) {
+                            Toast.makeText(requireContext(), "RIGHT SIDE WON!!", Toast.LENGTH_LONG).show();
+                            deckCount = 27;
 
-                //show card images
-                int leftImage = getResources().getIdentifier(leftShape + leftCard, "drawable", getActivity().getPackageName());
-                iv_card_left.setImageResource(leftImage);
-                int rightImage = getResources().getIdentifier(rightShape + rightCard, "drawable", getActivity().getPackageName());
-                iv_card_right.setImageResource(rightImage);
+                        } else {
+                            Toast.makeText(requireContext(), "TIED UP!!!", Toast.LENGTH_LONG).show();
+                            deckCount = 27;
 
-                //compare cards, add points and display them
-                if (leftCard > rightCard) {
-                    leftScore++;
-                    tv_score_left.setText(String.valueOf(leftScore));
-                } else if(leftCard < rightCard) {
-                    rightScore++;
-                    tv_score_right.setText(String.valueOf(rightScore));
-                } else {
-                    if (leftScore > rightScore) {
-                        Toast.makeText(requireContext(), "LEFT SIDE WIN!!", Toast.LENGTH_LONG).show();
-                    } else if (leftScore < rightScore) {
-                        Toast.makeText(requireContext(), "RIGHT SIDE WIN!!", Toast.LENGTH_LONG).show();
+                        }
+
+                    } else if (deckCount == 27) {
+//                    Toast.makeText(requireContext(), "GAME IS OVER!!! PLEASE RESET!!!", Toast.LENGTH_LONG).show();
+                        Toast.makeText(requireContext(), "RAN OUT OF CARDS!!! PLEASE RESET!!!", Toast.LENGTH_LONG).show();
+                    } else if (deckCount == 28) {
+                        Toast.makeText(requireContext(), "GAME IS OVER!!! PLEASE RESET!!!", Toast.LENGTH_LONG).show();
+                    } else if (deckCount < 26) {
+
+
+                        ///////////////NEW VERSION/////////////////////////
+                        //Shuffles exactly 52 cards
+
+                        int leftNumber = leftDeckNumbers.get(deckCount);
+                        int rightNumber = rightDeckNumbers.get(deckCount);
+                        String leftCard = leftDeck.get(deckCount);
+                        String rightCard = rightDeck.get(deckCount);
+
+                        //show card images
+                        int leftImage = getResources().getIdentifier(leftCard, "drawable", getActivity().getPackageName());
+                        iv_card_left.setImageResource(leftImage);
+                        int rightImage = getResources().getIdentifier(rightCard, "drawable", getActivity().getPackageName());
+                        iv_card_right.setImageResource(rightImage);
+
+                        deckCount++;
+
+                        //compare cards, add points and display them
+                        if (leftNumber > rightNumber) {
+//                        leftScore++;
+                            leftScore += betRight;
+                            rightScore -= betRight;
+//                            Toast.makeText(requireContext(), "LEFT WINS THE BET!", Toast.LENGTH_SHORT).show();
+//                            SystemClock.sleep(3000);
+                            tv_score_left.setText(String.valueOf(leftScore));
+                            tv_score_right.setText(String.valueOf(rightScore));
+                            String winner = earlyWinner();
+                            if (winner == "leftwon") {
+                                Toast.makeText(requireContext(), "LEFT SIDE WINS THE GAME!!!!!!", Toast.LENGTH_LONG).show();
+                                deckCount = 28;
+                            } else if (winner == "rightwon") {
+                                Toast.makeText(requireContext(), "RIGHT SIDE WINS THE GAME!!!!!!", Toast.LENGTH_LONG).show();
+                                deckCount = 28;
+                            }
+
+                        } else if (leftNumber < rightNumber) {
+//                        rightScore++;
+                            rightScore += betLeft;
+                            leftScore -= betLeft;
+//                            Toast.makeText(requireContext(), "RIGHT WINS THE BET!", Toast.LENGTH_SHORT).show();
+//                            SystemClock.sleep(3000);
+                            tv_score_right.setText(String.valueOf(rightScore));
+                            tv_score_left.setText(String.valueOf(leftScore));
+                            String winner = earlyWinner();
+                            if (winner == "leftwon") {
+                                Toast.makeText(requireContext(), "LEFT SIDE WINS THE GAME!!!!!!", Toast.LENGTH_LONG).show();
+                                deckCount = 28;
+                            } else if (winner == "rightwon") {
+                                Toast.makeText(requireContext(), "RIGHT SIDE WINS THE GAME!!!!!!", Toast.LENGTH_LONG).show();
+                                deckCount = 28;
+                            }
+
+                        } else {
+                            if (leftScore > rightScore) {
+                                Toast.makeText(requireContext(), "LEFT SIDE WINS THE GAME!!!!!!", Toast.LENGTH_LONG).show();
+                                deckCount = 28;
+                            } else if (leftScore < rightScore) {
+                                Toast.makeText(requireContext(), "RIGHT SIDE WINS THE GAME!!!!!!", Toast.LENGTH_LONG).show();
+                                deckCount = 28;
+                            } else {
+                                Toast.makeText(requireContext(), "TIED UP!!!", Toast.LENGTH_LONG).show();
+                                deckCount = 28;
+                            }
+
+                        }
                     } else {
-                        Toast.makeText(requireContext(), "TIED UP!!!", Toast.LENGTH_LONG).show();
+                        Toast.makeText(requireContext(), "COUNTING ERROR... PLEASE RESTART AVOCADO", Toast.LENGTH_LONG).show();
                     }
+                    betLeft = 0;
+                    betRight = 0;
+                    tv_bet_left.setText(String.valueOf(betLeft));
+                    tv_bet_right.setText(String.valueOf(betRight));
+
                 }
             }
         });
@@ -107,16 +232,23 @@ public class Tab3Fragment extends Fragment {
             @Override
             public void onClick(View view) {
                 //initialize scores
-                leftScore = 0;
+                leftScore = 50;
                 tv_score_left.setText(String.valueOf(leftScore));
-                rightScore = 0;
+                rightScore = 50;
                 tv_score_right.setText(String.valueOf(rightScore));
 
-                //initialize cards
+                shuffleDeck();
+
+                Toast.makeText(requireContext(), "DECK SHUFFLED!!!", Toast.LENGTH_SHORT).show();
+
+                //show back of the cards
                 int leftImage = getResources().getIdentifier("black_joker", "drawable", getActivity().getPackageName());
                 iv_card_left.setImageResource(leftImage);
                 int rightImage = getResources().getIdentifier("black_joker", "drawable", getActivity().getPackageName());
                 iv_card_right.setImageResource(rightImage);
+
+
+
 
             }
         });
@@ -124,5 +256,57 @@ public class Tab3Fragment extends Fragment {
 
 //        return inflater.inflate(R.layout.fragment_tab3, container, false);
     }
+
+    public void shuffleDeck(){
+        int cardnumber;
+        String cardshape;
+        Random rrr = new Random();
+        Random rrrr = new Random();
+        wholeDeck.clear();
+        leftDeck.clear();
+        rightDeck.clear();
+        leftDeckNumbers.clear();
+        rightDeckNumbers.clear();
+
+        deckCount = 0;
+
+//        if (wholeDeck != null) wholeDeck.clear();
+//        wholeDeck.add("START");
+        for (int i=0; i<52; i++) {
+            do {
+                cardnumber = rrr.nextInt(13) + 2; //this is for cards 2 - 14
+                cardshape = shapes[rrrr.nextInt(4)];
+            } while (wholeDeck.contains(cardshape + cardnumber));
+            wholeDeck.add(cardshape + cardnumber);
+            if (i < 26) {
+                leftDeck.add(cardshape + cardnumber);
+                leftDeckNumbers.add(cardnumber);
+            } else {
+                rightDeck.add(cardshape + cardnumber);
+                rightDeckNumbers.add(cardnumber);
+            }
+        }
+    }
+
+//    public String earlyWinner() {
+//        if (26-deckCount < leftScore-rightScore) {
+//            return "leftwon";
+//        } else if (26-deckCount < rightScore - leftScore) {
+//            return "rightwon";
+//        } else {
+//            return "";
+//        }
+//    }
+
+    public String earlyWinner() {
+        if (leftScore>rightScore && rightScore==0) {
+            return "leftwon";
+        } else if (leftScore<rightScore && leftScore==0) {
+            return "rightwon";
+        } else {
+            return "";
+        }
+    }
+
 }
 
